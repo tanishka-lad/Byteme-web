736f6d4ab9a17d7721e3d8fd0b12892b29c48d32
Tanishka Lad <ladtanishka@gmail.com>
2026-01-12 22:45:08 +0530

    Implement middleware for default user role cookie
    Added middleware to set default cookie for user role.

--- got_ctf_challenge/server.js
+++ got_ctf_challenge/server.js
@@ -6,11 +6,15 @@ const app = express();
 app.use(cookieParser());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    // Simply set a cookie that says "commoner"
+// Middleware to set default cookie on all routes
+app.use((req, res, next) => {
     if (!req.cookies.role) {
         res.cookie('role', 'commoner');
     }
+    next();
+});
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
@@ -20,7 +24,7 @@ app.get('/secret_vault', (req, res) => {
     if (userRole === 'Lord_Treasurer') {
         res.send("<h1>Success!</h1><p>The Iron Throne is yours. Flag: <b>ByteMe{Easier_Than_The_Long_Night}</b></p>");
     } else {
-        res.send(`<h1>Access Denied</h1><p>Your current role is '${userRole}'. Only the 'Lord_Treasurer' can enter.</p>`);
+        res.send(<h1>Access Denied</h1><p>Your current role is '${userRole}'. Only the 'Lord_Treasurer' can enter.</p>);
     }
 });
 
